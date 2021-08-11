2c603ff88b376de87ea4307ee52ffae5fa3d5a4b
branko-bajic <branko.bajic@gov.bc.ca>
2021-08-11 12:56:26 -0700

    Updating port to 8080

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })) //Parse URL-encoded bodies
 app.use(cors());
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081,
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use('/jobs', jobRouter);
