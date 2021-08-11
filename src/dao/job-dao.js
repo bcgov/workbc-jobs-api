f021fe8cf236cb9026e0f1e97ebf994b86c30a15
branko-bajic <branko.bajic@gov.bc.ca>
2021-08-11 12:38:20 -0700

    Making the code node 12 compliant

--- src/dao/job-dao.js
+++ src/dao/job-dao.js
@@ -2,18 +2,18 @@ var jobs = require('./json/job.json');
 
 exports.getJobs = async (params) => {
   try {
-    let jobTitle = params.jobTitle?.toLowerCase().trim();
+    let jobTitle = params.jobTitle ? params.jobTitle.toLowerCase().trim() : null;
     let minimumPostedDate = params.minimumPostedDate ? new Date(params.minimumPostedDate) : null;
 
     var results = jobs.jobs.filter(job => 
       // Job Title //
       (jobTitle ? 
-        job.jobTitle?.toLowerCase().includes(jobTitle) 
+        (job.jobTitle ? job.jobTitle.toLowerCase().includes(jobTitle) : false)
         : true) &&
 
       // Minimum Posted Date //
       (minimumPostedDate ? 
-        (job.postedDate? new Date(job.postedDate) >= minimumPostedDate : false)
+        (job.postedDate ? new Date(job.postedDate) >= minimumPostedDate : false)
         : true)
     );
 
@@ -38,8 +38,8 @@ exports.totalJobs = async () => {
 
 exports.searchJobs = async (params) => {
   try {
-    let jobTitle = params.jobTitle?.toLowerCase().trim();
-    let location = params.location?.toLowerCase().trim();
+    let jobTitle = params.jobTitle ? params.jobTitle.toLowerCase().trim() : null;
+    let location = params.location ? params.location.toLowerCase().trim() : null;
 
     var results = jobs.jobs.filter(job => 
       // Job Title //
@@ -48,9 +48,9 @@ exports.searchJobs = async (params) => {
       // Location //
       ((location && job.locations) ? job.locations.some(loc => {
         return (
-          loc.city?.toLowerCase().trim() == location ||
-          loc.region?.caption?.toLowerCase().trim() == location ||
-          loc.province?.toLowerCase().trim() == location
+          loc.city ? loc.city.toLowerCase().trim() == location : false ||
+          loc.region && loc.region.caption ? loc.region.caption.toLowerCase().trim() == location : false ||
+          loc.province ? loc.province.toLowerCase().trim() == location : false
         );
       }) : false)
     );
