8fe88ca9936d982a904863d037b9097e0ed31cc8
Branko Bajic <branko@mailfence.com>
2021-10-14 13:09:42 -0700

    Fix import to only write file when api call returns 200 code

--- src/scripts/import_job_listings.py
+++ src/scripts/import_job_listings.py
@@ -5,14 +5,18 @@ import sys
 
 def fetch(url):
     res = requests.post(url, json={ "lastRequestDate": "2015-08-29" })
-    #TODO: parse for only active jobs
-    return json.loads(res.content)
+    if (res.status_code == requests.codes.ok):
+        return json.loads(res.content) #TODO: parse for only active jobs
+    else:
+        return
+    
+    
 
 # run with: python3 import_job_listings.py
 if __name__ == '__main__':
     jobs = fetch(url='https://workbcjobs.api.gov.bc.ca/v1/jobs')
-    dirname = os.path.dirname(__file__)
-    path = os.path.join(dirname, '../dao/json/job.json')
-
-    with open(path, 'w') as outfile:
-        json.dump(jobs, outfile, indent=4)
+    if (jobs != None): # Only write to file if the api call was successful
+        dirname = os.path.dirname(__file__)
+        path = os.path.join(dirname, '../dao/json/job.json')
+        with open(path, 'w') as outfile:
+            json.dump(jobs, outfile, indent=4)
